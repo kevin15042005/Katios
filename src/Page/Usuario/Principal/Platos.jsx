30969558909dd393f4e77f76418e9e01647445f6
kevin15042005 <kevin2013118@gmail.com>
2026-06-04 12:31:48 -0500

    PNg a webp y navbar mejorado

--- src/Page/Usuario/Principal/Platos.jsx
+++ src/Page/Usuario/Principal/Platos.jsx
@@ -1,33 +1,33 @@
 import ImagenInterGibhi from "../../../assets/Inter/KatiosInter.png";
-import Inter1 from "../../../assets/Inter/Inter1.png";
-import Inter2 from "../../../assets/Inter/Inter2.png";
-import Inter3 from "../../../assets/Inter/Inter3.png";
+import Inter1 from "../../../assets/Inter/Inter1.webp";
+import Inter2 from "../../../assets/Inter/Inter2.webp";
+import Inter3 from "../../../assets/Inter/Inter3.webp";
 
-import ImagenRt11Gibhi from "../../../assets/Rt11/KatiosRt11.png";
-import Rt11 from "../../../assets/Rt11/Rt11.png"
-import Rt111 from "../../../assets/Rt11/Rt111.png"
-import Rt112 from "../../../assets/Rt11/Rt112.png"
-
-
+import ImagenRt11Gibhi from "../../../assets/Rt11/KatiosRt11.webp";
+import Rt11 from "../../../assets/Rt11/Rt11.webp"
+import Rt111 from "../../../assets/Rt11/Rt111.webp"
+import Rt112 from "../../../assets/Rt11/Rt112.webp"
 
 
-import ImagenPuenteGibhi from "../../../assets/Puente/KatiosPuente.png";
-import Puente1 from "../../../assets/Puente/Puente1.png"
-import Puente2 from "../../../assets/Puente/Puente2.png"
 
 
-import ImagenPlazoletaGibhi from "../../../assets/Plazoleta/KatiosPlazoleta.png";
-import Plazoleta1 from "../../../assets/Plazoleta/Plazoleta2.png";
-import Plazoleta2 from "../../../assets/Plazoleta/Plazoleta2.png";
-import Plazoleta3 from "../../../assets/Plazoleta/Plazoleta3.png";
+import ImagenPuenteGibhi from "../../../assets/Puente/KatiosPuente.webp";
+import Puente1 from "../../../assets/Puente/Puente1.webp"
+import Puente2 from "../../../assets/Puente/Puente2.webp"
 
 
-import ImagenToGo from "../../../assets/ToGo/KatiosToGo.png";
-import ToGo1 from "../../../assets/ToGo/Inter1.png";
+import ImagenPlazoletaGibhi from "../../../assets/Plazoleta/KatiosPlazoleta.webp";
+import Plazoleta1 from "../../../assets/Plazoleta/Plazoleta2.webp";
+import Plazoleta2 from "../../../assets/Plazoleta/Plazoleta2.webp";
+import Plazoleta3 from "../../../assets/Plazoleta/Plazoleta3.webp";
 
 
-import ImagenFuncionarios from "../../../assets/Funcionarios/Plazoleta2.png";
-import Funcionario1 from "../../../assets/Funcionarios/Plazoleta2.png";
+import ImagenToGo from "../../../assets/ToGo/KatiosToGo.webp";
+import ToGo1 from "../../../assets/ToGo/Inter1.webp";
+
+
+import ImagenFuncionarios from "../../../assets/Funcionarios/Plazoleta2.webp";
+import Funcionario1 from "../../../assets/Funcionarios/Plazoleta2.webp";
 
 import Tarjeta from "./Tarjeta";
 export default function Platos() {
