32f93b313cd1dee94acf17015189059c0706737e
kevin15042005 <kevin2013118@gmail.com>
2026-06-04 12:41:39 -0500

    Slogan mehorado con imagen

--- src/Page/Usuario/Principal/Descripcion.jsx
+++ src/Page/Usuario/Principal/Descripcion.jsx
@@ -1,13 +1,13 @@
-import DescripcionLogo from "../../../assets/Puente/KatiosPuente.webp";
+import DescripcionLogo from "../../../assets/LogoSlogan.png";
 export default function Descripcion() {
   return (
     <>
       <div className="bg-[#ccba7c] flex justify-center mb-12 py-20 ">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5 items-center py-5 ">
           <div className="flex justify-center">
-            <img src={DescripcionLogo} alt="" className="rounded-full" />
+            <img src={DescripcionLogo} alt="" />
           </div>
-          <div className="w-80 md:W-110 text-[16px] md:text-[22px] font-bold font-stretch-expanded">
+          <div className="w-80 md:w-110 text-[16px] md:text-[22px]  m-auto font-bold font-stretch-expanded">
             <p className="text-[#ad8433]">
               Somos una familia que inspirada en la esencia de lo nuestro,
               ofrece la mejor Parrilla y Comida Típica Colombiana, con nuestra
