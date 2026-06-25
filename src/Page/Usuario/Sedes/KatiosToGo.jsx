843bc553ab1534eb94134866cc05a5dd9600a45a
german551 <german93_973@wikey.lat>
2026-06-25 14:29:05 -0500

    Arreglo timestampz

--- src/Page/Usuario/Sedes/KatiosToGo.jsx
+++ src/Page/Usuario/Sedes/KatiosToGo.jsx
@@ -91,7 +91,7 @@ export default function KatiosToGo() {
               {carta.pages?.map((img, i) => (
                 <div key={i}>
                   <img
-                    src={`${import.meta.env.VITE_API_URL}${img}?t=${timestampzs}`}
+                    src={`${import.meta.env.VITE_API_URL}${img}?t=${timestamps}`}
                     style={{
                       width: "100%",
                       height: "100%",
