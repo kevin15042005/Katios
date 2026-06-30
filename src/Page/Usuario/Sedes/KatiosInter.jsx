93ffd6323a9f75aef266b0849fd976e649acddc6
german551 <german93_973@wikey.lat>
2026-06-30 14:26:57 -0500

    Devuelta la normlaidad

--- src/Page/Usuario/Sedes/KatiosInter.jsx
+++ src/Page/Usuario/Sedes/KatiosInter.jsx
@@ -76,41 +76,38 @@ export default function KatiosInter() {
         </div>
       ) : (
         <div className="flex flex-col items-center gap-16 w-full max-w-full overflow-hidden isolate my-10">
-  {cartas.map((carta, index) => (
-    <React.Fragment key={carta.id}>
-      <h2 className="text-[16px] md:text-3xl">{index === 0 ? "Español" : "English"}</h2>
-
-      <HTMLFlipBook
-        key={`${carta.id}-${timestamp}`}
-        width={450}
-        height={650}
-        size="stretch"
-        showCover={true}
-        minWidth={250}
-        maxWidth={500}
-        minHeight={650}
-        maxHeight={650}
-        usePortrait={true}
-        startPage={0}
-        mobileScrollSupport={true}
-        className="mx-auto"
-      >
-        {carta.pages?.map((img, i) => (
-          <div key={i}>
-            <img
-              src={`${import.meta.env.VITE_API_URL}${img}?t=${timestamp}`}
-              style={{
-                width: "100%",
-                height: "100%",
-                objectFit: "contain",
-              }}
-            />
-          </div>
-        ))}
-      </HTMLFlipBook>
-    </React.Fragment>
-  ))}
-</div>
+          {" "}
+          {cartas.map((carta) => (
+            <HTMLFlipBook
+              key={`${carta.id}-${timestamp}`}
+              width={450}
+              height={650}
+              size="stretch"
+              showCover={true}
+              minWidth={250}
+              maxWidth={500}
+              minHeight={650}
+              maxHeight={650}
+              usePortrait={true}
+              startPage={0}
+              mobileScrollSupport={true}
+              className="mx-auto"
+            >
+              {carta.pages?.map((img, i) => (
+                <div key={i}>
+                  <img
+                    src={`${import.meta.env.VITE_API_URL}${img}?t=${timestamp}`}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "contain",
+                    }}
+                  />
+                </div>
+              ))}
+            </HTMLFlipBook>
+          ))}
+        </div>
       )}
       <div className="col-span-1 md:col-span-2 flex justify-center my-10">
         <iframe
