6933a8f8ec865e40b863f993da33f2cca018a4e6
german551 <german93_973@wikey.lat>
2026-06-30 14:22:05 -0500

    Carta en espanol y ingles1.2

--- src/Page/Usuario/Sedes/KatiosInter.jsx
+++ src/Page/Usuario/Sedes/KatiosInter.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import HTMLFlipBook from "react-pageflip";
+import { Fragment } from "react";
 import Parilla from "@/assets/Inter/KatiosInter.webp";
 
 export default function KatiosInter() {
@@ -76,43 +77,41 @@ export default function KatiosInter() {
         </div>
       ) : (
         <div className="flex flex-col items-center gap-16 w-full max-w-full overflow-hidden isolate my-10">
-          {cartas.map((carta, index) => (
-            <div key={carta.id} className="flex flex-col items-center">
-              <h2 className="text-3xl font-bold mb-5">
-                {index === 0 ? "Carta - Español" : "Carta - English"}
-              </h2>
+  {cartas.map((carta, index) => (
+    <React.Fragment key={carta.id}>
+      <h2>{index === 0 ? "Español" : "English"}</h2>
 
-              <HTMLFlipBook
-                key={`${carta.id}-${timestamp}`}
-                width={450}
-                height={650}
-                size="stretch"
-                showCover={true}
-                minWidth={250}
-                maxWidth={500}
-                minHeight={650}
-                maxHeight={650}
-                usePortrait={true}
-                startPage={0}
-                mobileScrollSupport={true}
-                className="mx-auto"
-              >
-                {carta.pages?.map((img, i) => (
-                  <div key={i}>
-                    <img
-                      src={`${import.meta.env.VITE_API_URL}${img}?t=${timestamp}`}
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "contain",
-                      }}
-                    />
-                  </div>
-                ))}
-              </HTMLFlipBook>
-            </div>
-          ))}
-        </div>
+      <HTMLFlipBook
+        key={`${carta.id}-${timestamp}`}
+        width={450}
+        height={650}
+        size="stretch"
+        showCover={true}
+        minWidth={250}
+        maxWidth={500}
+        minHeight={650}
+        maxHeight={650}
+        usePortrait={true}
+        startPage={0}
+        mobileScrollSupport={true}
+        className="mx-auto"
+      >
+        {carta.pages?.map((img, i) => (
+          <div key={i}>
+            <img
+              src={`${import.meta.env.VITE_API_URL}${img}?t=${timestamp}`}
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "contain",
+              }}
+            />
+          </div>
+        ))}
+      </HTMLFlipBook>
+    </React.Fragment>
+  ))}
+</div>
       )}
       <div className="col-span-1 md:col-span-2 flex justify-center my-10">
         <iframe
